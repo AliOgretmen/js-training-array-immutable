3167d453699480b38b511929be5b958ebca675c9
AliOgretmen <aliogretmen.ch@gmail.com>
2017-09-09 14:17:59 +0200

    resolved

--- js/chapters/chapter-02.js
+++ js/chapters/chapter-02.js
@@ -22,6 +22,44 @@
  * - The decleration order of functions is not important!
  */
 
+function findNumberOfPassengersByMinChildren(pPassengers, pNoOfChildren){
+    return pPassengers.filter(person => person.children.length >= pNoOfChildren).length;
+}
+function findNumberPassengersByMinChildrenAndGender(pPassengers, pNoOfChildren, pGender){
+    return pPassengers.filter(person => person.gender === pGender && person.children.length >= 2).length;
+}
+function findNumberOfPassengersByCountry(pPassengers, pCountry){
+    return pPassengers.filter(person => person.addres.country == pCountry).length;
+}
+function findNumberOfPassengersByChildrenAndCountry(pPassengers, pNoOfChildren, pCountry){
+    return pPassengers.filter(person => person.addres.country === "Swiss" ).filter(person => person.children.length == 0).length;
+}
+function findNumberOfPassengersByLanguageAndGender(pPassengers, pLanguage, pGender){
 
-
-//coming soon...
+    return pPassengers.filter(person => person.gender === pGender).filter(person => person.languages.find(x => x=== pLanguage)).length;
+}
+function findNumberOfPassengersByLanguageAndGenderAndCountry(pPassengers, pLanguage, pGender, pCountry){
+    return pPassengers.filter(person => person.addres.country == pCountry).filter(person => person.gender === pGender).filter(person => person.languages.find(x => x=== pLanguage)).length;
+}
+function findNumberOfPassengersByLanguageAndSalaryAndNumberOfChildren(pPassengers, pLanguage, pSalary, pNoOfChildren){
+    return pPassengers.filter(person => person.languages.find(x => x=== pLanguage)).filter(person => person.children.length == 0).filter(person => person.salary >= 61000).length;
+}
+function isThereAnyPersonBySalaryAndCountry(pPassengers, pSalary, pCountry){
+    return pPassengers.filter(person => person.addres.country == pCountry).filter(person => person.salary < 50000).length > 0;
+}
+function findNumberOfPassengersByEntryDate(pPassengers, pEntryDate){
+    return pPassengers.filter(person => person.entryDateInSwiss >= pEntryDate).length;
+}
+function findNamesOfPassengersByOrigin(pPassengers, pOrigin){
+    var adanalilar = pPassengers.filter(person => person.origin === pOrigin);
+    var result = adanalilar.map(function (a) {
+        return a.firstname;
+    });
+    return result;
+}
+function findNamesOfPassengersByHoroscope(pPassengers, pHoroscopeSign){
+    return pPassengers;    
+}
+function findNamesOfPassengersByAcceptedAndNotAcceptedLanguages(pPassengers, pAccesptedLanguages, pNotAcceptedLanguages){
+    return pPassengers.filter(person => person.languages.find(pAccesptedLanguages));
+}
